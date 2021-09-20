e7a0b09e93f4aa15611a17f49a37a0cfb1e8921c
padmini v <padmini.v@perfios.com>
2021-09-20 22:23:57 +0530

    ADDED: OpenCSVWriter class to write into csv file

---
+++ src/main/java/OpenCSVAndGson/OpenCSVWriter.java
@@ -0,0 +1,13 @@
+package OpenCSVAndGson;
+
+public class OpenCSVWriter {
+
+	private static final String OBJECT_LIST_SAMPLE="./object-list-sample.csv";
+	
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
