66f0f2e20ada4ce826a8858e1396677918867d0b
padmini v <padmini.v@perfios.com>
2021-09-20 22:28:43 +0530

    MODIFIED: reading all records at once

--- src/main/java/OpenCSVAndGson/OpenCSVReader.java
+++ src/main/java/OpenCSVAndGson/OpenCSVReader.java
@@ -10,21 +10,21 @@ import com.opencsv.CSVReader;
 
 public class OpenCSVReader 
 {
-    
 	 public static final String CSV_FILE_PATH="./users.csv";
-
-	    public static void main(String[] args) throws IOException
+	    
+	    public static void main(String[] args)throws IOException
 	    {
 	    	try (
 	    		Reader reader=Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
 	    		CSVReader csvReader=new CSVReader(reader);
 	    	){
-	    		String[] nextRecord;
-	    		while((nextRecord = csvReader.readNext())!=null) {
-	    			System.out.println("Name: "+ nextRecord[0]);
-	    			System.out.println("Email: "+ nextRecord[1]);
-	    			System.out.println("Phone: "+ nextRecord[2]);
-	    			System.out.println("Country: "+ nextRecord[3]);
+	    		List<String[]> records=csvReader.readAll();
+	    		for(String[] record : records) 
+	    		{
+	    			System.out.println("Name: "+ record[0]);
+	    			System.out.println("Email: "+ record[1]);
+	    			System.out.println("Phone: "+ record[2]);
+	    			System.out.println("Country: "+ record[3]);
 	    			System.out.println("==================");
 	    		}
 	    	}
